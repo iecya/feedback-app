bf55a8b9eb52d531489ff9c325081db7baaaa7ba
iecya <federica.delisi@hotmail.it>
2019-09-02 14:02:08 +0100

    Refactor header component

--- server/client/src/components/Header.js
+++ server/client/src/components/Header.js
@@ -2,7 +2,22 @@ import React, { Component } from 'react';
 
 class Header extends Component {
     render() {
-        return <div>Header</div>;
+        return (
+            <div>
+                <nav>
+                    <div className="nav-wrapper">
+                        <a href="/" className="brand-logo">
+                            Emaily
+                        </a>
+                        <ul className="right">
+                            <li>
+                                <a>Login with Google</a>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+            </div>
+        );
     }
 }
 
