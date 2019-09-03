9116f651c69bad851ba0e26ede1ac13a79fbdf91
iecya <federica.delisi@hotmail.it>
2019-09-03 11:01:57 +0100

    Dispatch action on user payment

--- server/client/src/components/Payments.js
+++ server/client/src/components/Payments.js
@@ -1,16 +1,17 @@
 import React, { Component } from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
 
 class Payments extends Component {
     render() {
-        debugger;
         return (
             <StripeCheckout
                 // amount in cents
                 amount={500}
                 name="Emaily"
                 description="$5 for 5 email credits"
-                token={token => console.log(token)}
+                token={token => this.props.handleToken(token)}
                 stripeKey={process.env.REACT_APP_STRIPE_KEY}
             >
                 <button className="btn">Add credits</button>
@@ -19,4 +20,7 @@ class Payments extends Component {
     }
 }
 
-export default Payments;
+export default connect(
+    null,
+    actions
+)(Payments);
