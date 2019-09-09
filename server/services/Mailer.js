f039da3c69cc6b7497e6d7d813251c1aa7075f32
iecya <federica.delisi@hotmail.it>
2019-09-09 12:43:42 +0100

    Create Mailer class

---
+++ server/services/Mailer.js
@@ -0,0 +1,43 @@
+const sendgrid = require('sendgrid');
+const helper = sendgrid.mail;
+const { sendGridKey } = require('../config/keys');
+
+class Mailer extends helper.Mail {
+    constructor({ subject, recipients }, content) {
+        super();
+
+        this.from_email = new helper.Email('no-reply@emaily.com');
+        this.subject = subject;
+        this.body = new helper.Content('text/html', content);
+        this.recipients = this.formatAddresses(recipients);
+
+        // this.addContent is a method provided by the helper.Mail class
+        this.addContent(this.body);
+        this.addClickTracking();
+        this.addRecipients();
+    }
+
+    formatAddresses(recipients) {
+        return recipients.map(({ email }) => {
+            return new helper.Email(email);
+        });
+    }
+
+    addClickTracking() {
+        const trackingSettings = new helper.TrackingSettings();
+        const clickTracking = new helper.ClickTRacking(true, true);
+
+        trackingSettings.setClickTracking(clickTracking);
+        this.addTrackingSettings(trackingSettings);
+    }
+
+    addRecipients(recipients) {
+        const personalize = new helper.Personalization();
+        this.recipients.forEach(recipient => {
+            personalize.addTo(recipient);
+        });
+        this.addPersonalization(personalize);
+    }
+}
+
+module.exports = Mailer;
