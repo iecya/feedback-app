2fe94e9f373917c9ce1a89c1c66f9da6171dc4a0
iecya <federica.delisi@hotmail.it>
2019-09-09 13:59:34 +0100

    User redirection when voting

--- server/services/emailTemplates/surveyTemplate.js
+++ server/services/emailTemplates/surveyTemplate.js
@@ -1,3 +1,5 @@
+const { redirectDomain } = require('../../config/keys');
+
 module.exports = survey => {
     return `
       <html>
@@ -7,12 +9,11 @@ module.exports = survey => {
             <p>Please answer the following question:</p>
             <p>${survey.body}</p>
             <div>
-              <a href="http://localhost:3000">Yes</a>
+              <a href="${redirectDomain}/api/surveys/thanks">Yes</a>
             </div>
             <div>
-              <a href="http://localhost:3000">No</a>
+              <a href="${redirectDomain}/api/surveys/thanks">No</a>
             </div>
-
           </div>
         </body>
       </html>
