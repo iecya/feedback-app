288f6ea82532431a7e8a290415083038aadcef31
iecya <federica.delisi@hotmail.it>
2019-09-13 13:27:25 +0100

    Create action for survey submit

--- server/client/src/components/surveys/SurveyFormReview.js
+++ server/client/src/components/surveys/SurveyFormReview.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import fieldsConfig from './formFields';
+import * as actions from '../../actions';
 
-const SurveyReview = ({ onCancel, formValues }) => {
+const SurveyReview = ({ onCancel, formValues, submitSurvey }) => {
     const reviewFields = fieldsConfig.map(({ name, label }) => {
         return (
             <div key={name}>
@@ -16,8 +17,12 @@ const SurveyReview = ({ onCancel, formValues }) => {
         <div>
             <h5>Please confirm your entries</h5>
             <div>{reviewFields}</div>
-            <button className="yellow darken-3 btn-flat" onClick={onCancel}>
+            <button className="yellow darken-3 btn-flat white-text" onClick={onCancel}>
                 Back
+            </button>
+            <button className="green btn-flat right white-text" onClick={() => submitSurvey(formValues)}>
+                Send Survey
+                <i className="material-icons right">email</i>
             </button>
         </div>
     );
@@ -27,4 +32,7 @@ function mapStateToProps(state) {
     return { formValues: state.form.surveyForm.values };
 }
 
-export default connect(mapStateToProps)(SurveyReview);
+export default connect(
+    mapStateToProps,
+    actions
+)(SurveyReview);
