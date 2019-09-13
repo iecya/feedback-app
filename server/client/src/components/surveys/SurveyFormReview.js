9a092e3589b4bb888ba33296cc4df9855cd174cb
iecya <federica.delisi@hotmail.it>
2019-09-13 13:11:07 +0100

    Render review field for each field in fieldsConfig

--- server/client/src/components/surveys/SurveyFormReview.js
+++ server/client/src/components/surveys/SurveyFormReview.js
@@ -3,15 +3,19 @@ import { connect } from 'react-redux';
 import fieldsConfig from './formFields';
 
 const SurveyReview = ({ onCancel, formValues }) => {
+    const reviewFields = fieldsConfig.map(({ name, label }) => {
+        return (
+            <div key={name}>
+                <label>{label}</label>
+                <div>{formValues[name]}</div>
+            </div>
+        );
+    });
+
     return (
         <div>
             <h5>Please confirm your entries</h5>
-            <div>
-                <div>
-                    <lavel>Survey Title</lavel>
-                    <div>{formValues.title}</div>
-                </div>
-            </div>
+            <div>{reviewFields}</div>
             <button className="yellow darken-3 btn-flat" onClick={onCancel}>
                 Back
             </button>
