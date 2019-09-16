0758b0586a8d7b1672023843aa1e23d0fb99e99e
iecya <federica.delisi@hotmail.it>
2019-09-16 17:39:30 +0100

    Create cards for each survey

--- server/client/src/components/surveys/SurveyList.js
+++ server/client/src/components/surveys/SurveyList.js
@@ -6,8 +6,27 @@ class SurveyList extends Component {
     componentDidMount() {
         this.props.fetchSurveys();
     }
+
+    renderSurveys() {
+        return this.props.surveys.reverse().map(survey => {
+            return (
+                <div className="card blue-grey" key="survey._id">
+                    <div className="card-content white-text">
+                        <span className="card-title">{survey.title}</span>
+                        <p>{survey.body}</p>
+                        <p className="right">Sent on: {new Date(survey.dateSent).toLocaleDateString()}</p>
+                    </div>
+                    <div className="card-action">
+                        <a>Yes: {survey.yes}</a>
+                        <a>No: {survey.no}</a>
+                    </div>
+                </div>
+            );
+        });
+    }
+
     render() {
-        return <div>Survey list</div>;
+        return <div>{this.renderSurveys()}</div>;
     }
 }
 
